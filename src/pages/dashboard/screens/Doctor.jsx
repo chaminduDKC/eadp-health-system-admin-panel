b0682545113f13bdf24bc1c0d646324a4ee60172
chaminduDKC <chamindud061.com>
2025-06-28 18:21:44 +0530

    Save user completed

--- src/pages/dashboard/screens/Doctor.jsx
+++ src/pages/dashboard/screens/Doctor.jsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import '../../../styles/theme.css'
 import './doctor.css'
+import axios from "axios";
 import {useOutletContext} from "react-router-dom";
 import {Box, Button, Checkbox, FormControlLabel, MenuItem, Paper, TextField, Typography} from "@mui/material";
 
@@ -11,7 +12,15 @@ const Doctor = ()=>{
     const [lName, setLName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [specialization, setSpecialization] = useState("")
+    const [phone, setPhone] = useState("")
+    const [address, setAddress] = useState("")
     const [showPassword, setShowPassword] = useState(false)
+    const [hospital, setHospital] = useState("")
+    const [city, setCity] = useState("")
+    const [experience, setExperience] = useState("1");
+    const [licenceNo, setLicenceNo] = useState("");
+
     const hospitals = [
         {
             value:1,
@@ -45,10 +54,6 @@ const Doctor = ()=>{
             city:"Deniyaya",
         },
     ]
-    // add a note * user service inputs
-                // doc inputs
-                // pat inputs
-                // responses should include user id and other all data
     const doctorData = [
         {
             id:1,
@@ -78,12 +83,79 @@ const Doctor = ()=>{
             status:true
         },
     ]
+    const experiences = ["1", "2", "3", "4", "5", "5+"]
+    const [doctors, setDoctors] = useState([]);
+    const [error, setError] = useState(null);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            console.log("Step 1")
+            // Step 1: Prepare user data for user-service
+            const userRequest = {
+                email: email,
+                firstName:fName,
+                lastName: lName,
+                password: password,
+                // role: "doctor"
+            };
+
+            console.log("Step 2")
+            console.log(userRequest)
+            // Step 2: Send request to user-service
+            const userResponse = await axios.post(
+                "http://localhost:9090/api/users/register-doctor",
+                userRequest,
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+                    },
+                }
+            );
+            console.log(userResponse.data);
+
+            const userId = userResponse.data.data.userId;
+            const name = userResponse.data.data.firstName + " " + userResponse.data.data.lastName
+
+            // Step 3: Prepare doctor data for doctor-service
+            const doctorRequest = {
+                userId: userId,
+                name:name,
+                email: email,
+                phoneNumber: phone,
+                specialization:specialization,
+                experience: experience,
+                hospital: hospital,
+                address: address,
+                licenseNo: licenceNo,
+                city: city
+            };
+
+            // Step 4: Send request to doctor-service
+            await axios.post(
+                "http://localhost:9091/api/doctors/create-doctor",
+                doctorRequest,
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+                    },
+                }
+            );
+
+            alert("Doctor registered successfully!");
+        } catch (error) {
+            console.error("Error creating doctor:", error);
+            alert("Doctor registration failed. Please check the data and try again.");
+        }
+    };
+
     return(
         <div className={sideBarVisible ? "collapsed" : "expanded"}>
            <div className="inner doctor">
-               <Box sx={{backgroundColor:"var(--bg-primary)"}} maxWidth={500} ml={5} mt={5} p={3} border="1px solid var(--accent-color)" borderRadius={2}>
+               <Box sx={{backgroundColor:"var(--bg-primary)"}} mx="auto" maxWidth={800}  mt={5} p={3} border="1px solid var(--accent-color)" borderRadius={2}>
                    <Typography sx={{color:"var(--text-primary)", fontWeight:"bold"}} variant="h4" mb={2}>Register User</Typography>
-                   <form >
+                   <form onSubmit={handleSubmit}>
                        <TextField
                            sx={{
                                backgroundColor:"whitesmoke"
@@ -160,48 +232,63 @@ const Doctor = ()=>{
                                          sx={{color:"var(--text-primary)"}}
 
                        />
-                       <Button type="submit" variant="contained" sx={{backgroundColor:"var(--accent-color)", marginTop:"10px"}} fullWidth={true}>Save User</Button>
-                   </form>
-              </Box>
 
-               <Box sx={{backgroundColor:"var(--bg-primary)"}} maxWidth={1000} ml={5} mt={5} p={3} border="1px solid var(--accent-color)" borderRadius={2}>
-                   <Typography sx={{color:"var(--text-primary)", fontWeight:"bold"}} variant="h4" mb={2}>Complete doctor details</Typography>
-                   <form >
+
                        <Box sx={{display:"flex", width:"100%", justifyContent:"space-between", gap:"10px"}}>
-                       <TextField
-                           sx={{
-                               // width:"45%",
-                               backgroundColor:"whitesmoke"
-                           }
-                           }
-                           label="Phone"
-                           variant="filled"
-                           margin="normal"
-                           fullWidth
-                           required
-                           type="text"
-                           value={fName}
-                           onChange={(e => setFName(e.target.value))}
+                           <TextField
+                               sx={{
+                                   // width:"45%",
+                                   backgroundColor:"whitesmoke"
+                               }
+                               }
+                               label="Phone"
+                               variant="filled"
+                               margin="normal"
+                               fullWidth
+                               required
+                               type="text"
+                               value={phone}
+                               onChange={(e => setPhone(e.target.value))}
 
-                       />
-                       <TextField
-                           sx={{
+                           />
 
-                               backgroundColor:"whitesmoke"
-                           }
-                           }
-                           fullWidth
-                           label="City"
-                           variant="filled"
-                           margin="normal"
+                           <TextField
+                               sx={{
+                                   // width:"45%",
+                                   backgroundColor:"whitesmoke"
+                               }
+                               }
+                               label="Licence No"
+                               variant="filled"
+                               margin="normal"
+                               fullWidth
+                               required
+                               type="text"
+                               value={licenceNo}
+                               onChange={(e => setLicenceNo(e.target.value))}
 
-                           required
-                           type="text"
-                           value={email}
-                           onChange={(e => setEmail(e.target.value))}
+                           />
 
-                       />
+                           <TextField
+                               sx={{
+
+                                   backgroundColor:"whitesmoke"
+                               }
+                               }
+                               fullWidth
+                               label="Specialization"
+                               variant="filled"
+                               margin="normal"
+
+                               required
+                               type="text"
+                               value={specialization}
+                               onChange={(e => setSpecialization(e.target.value))}
+
+                           />
                        </Box>
+
+
                        <TextField
                            sx={{
                                backgroundColor:"whitesmoke"
@@ -213,8 +300,8 @@ const Doctor = ()=>{
                            fullWidth
                            required
                            type="text"
-                           value={lName}
-                           onChange={(e => setLName(e.target.value))}
+                           value={address}
+                           onChange={(e => setAddress(e.target.value))}
 
                        />
 
@@ -231,7 +318,8 @@ const Doctor = ()=>{
                                label="Select"
                                variant="filled"
                                fullWidth
-                               defaultValue="Colombo"
+                               onChange={(e)=> setHospital(e.target.value)}
+                               defaultValue={hospital}
                                helperText="Please select hospital"
                            >
                                {hospitals.map((option) => (
@@ -250,7 +338,8 @@ const Doctor = ()=>{
                                variant="filled"
                                fullWidth
                                label="Select"
-                               defaultValue="Colombo"
+                               defaultValue={city}
+                               onChange={(e)=> setCity(e.target.value)}
                                helperText="Please select city"
                            >
                                {cities.map((option) => (
@@ -259,16 +348,42 @@ const Doctor = ()=>{
                                    </MenuItem>
                                ))}
                            </TextField>
+
+
+                           <TextField
+                               sx={{
+                                   backgroundColor:"whitesmoke"
+                               }
+                               }
+                               id="outlined-select-experience"
+                               select
+                               variant="filled"
+                               fullWidth
+                               label="Select"
+                               defaultValue={experience}
+                               onChange={(e) => setExperience(e.target.value)}
+                               helperText="Please select experience in years"
+                           >
+                               {experiences.map((option) => (
+                                   <MenuItem >
+                                       {option}
+                                   </MenuItem>
+                               ))}
+                           </TextField>
+
+
                        </Box>
 
-                       <Button type="submit" variant="contained" sx={{backgroundColor:"var(--accent-color)", marginTop:"10px"}} fullWidth={true}>Save details</Button>
+
+
+                       <Button type="submit" variant="contained" sx={{backgroundColor:"var(--accent-color)", marginTop:"10px"}} fullWidth={true}>Save User</Button>
                    </form>
-               </Box>
+              </Box>
 
            </div>
 
             <div className="doc-details">
-                <Button color="success" sx={{marginTop:"20px", marginLeft:"40px"}} variant="contained">Load all doctors</Button>
+                <Button color="success" sx={{marginTop:"20px", marginLeft:"40px"}} onClick={()=> getAllDoctors()} variant="contained">Load all doctors</Button>
                 <Box sx={{
                     marginLeft:"40px",
                     marginTop:"20px"
